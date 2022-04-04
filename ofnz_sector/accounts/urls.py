2d4f51eec665f32c313e67d4b6774195ef2fc70b
mirooka <95923588+mirooka@users.noreply.github.com>
2022-04-04 11:50:16 +0300

    added views

--- ofnz_sector/accounts/urls.py
+++ ofnz_sector/accounts/urls.py
@@ -1,5 +1,11 @@
 from django.urls import path
+from django.views.generic import RedirectView
 
 urlpatterns = (
+    path('login/', UserLoginView.as_view(), name='Login user'),
+    path('edit-pass/', ChangeUserPasswordView.as_view(), name='change password'),
+    path('password-change-done/', RedirectView.as_view(), name='change password done'),
 
+    path('<int:pk>/', ProfileDetailsView.as_view(), name='profile details'),
+    path('register/', ProfileRegisterView.as_view(), name='profile register'),
 )
--- ofnz_sector/accounts/views.py
+++ ofnz_sector/accounts/views.py
@@ -1,3 +1,26 @@
+from django.urls import reverse_lazy
+from django.views import generic as views
+from django.contrib.auth import views as auth_views
+
 from django.shortcuts import render
 
+
 # Create your views here.
+class UserRegisterView(views.CreateView):
+    form_class = CreateProfileForm
+    template_name = 'accounts/profile_create.html'
+    success_url = reverse_lazy('index')
+
+
+class UserLoginView(auth_views.LoginView):
+    template_name = 'accounts/login_page.html'
+    success_url = reverse_lazy('index')
+
+    def get_success_url(self):
+        if self.success_url:
+            return self.success_url
+        return super().get_success_url()
+
+
+class EditProfileView:
+    pass
