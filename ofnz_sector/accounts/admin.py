fdb6c03a6b4054dbbfa2840b529e9a00037127d0
mirooka <95923588+mirooka@users.noreply.github.com>
2022-04-20 18:45:14 +0300

    added admin groups

--- ofnz_sector/accounts/admin.py
+++ ofnz_sector/accounts/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import OfnzUser, Profile
+
+admin.site.site_header = 'OfnzSector Administration'
+
+
+class OfnzUserAdmin(admin.ModelAdmin):
+    # fields = ('__all__',)
+    exclude = ('password', )
+
+admin.site.register(OfnzUser, OfnzUserAdmin)
+admin.site.register(Profile)
